58069eec05624f5b4dd6587345401ed445c93392
Denis Chakyr <ChakyrIT@gmail.com>
2023-03-19 22:49:44 +0200

    Sprint 08 - minor changes

--- pinterestClone/ImageList/ImageListCell.swift
+++ pinterestClone/ImageList/ImageListCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
