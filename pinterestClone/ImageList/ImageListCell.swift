f1ca3e03f526ddd135fddefd601298e53734a5de
Denis Chakyr <ChakyrIT@gmail.com>
2023-03-24 18:53:50 +0200

    Sprint 8 - Commit # 2
    1) Logo on LaunchScreen was optimized according to Figma
    2) LikeButton was reworked according to Figma sizes
    3) Fonts are corrected
    4) Logic of LikeButton (white and red hearts) was changed - now its working ))
    
    5) Status bar now is white
    6) Background of Cell is LightGrey now (because it's important to see tap-action)
    7) Was made GradientLayer on the bottom of Cell

--- pinterestClone/ImageList/ImageListCell.swift
+++ pinterestClone/ImageList/ImageListCell.swift
@@ -7,8 +7,34 @@
 
 import UIKit
 final class ImagesListCell: UITableViewCell {
+    //MARK: - Public Properties
     static let reuseIdentifier = "ImagesListCell"
+    var gradientLayer: CAGradientLayer!
+    //MARK: - IBOutlet
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
+    
+    //MARK: - Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO: - градиентный слой (✅DONE)
+        ///Задаем градиентный слой ячейкам
+        gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.YPGradient0?.cgColor as Any, UIColor.YPGradient20?.cgColor as Any]
+        gradientLayer.locations = [0.0, 1.0]
+        
+        //Установливаем границы градиента
+        gradientLayer.frame = CGRect(x: 0, y: cellImage.bounds.height - 35, width: cellImage.bounds.width, height: 35)
+//        gradientLayer.frame = CGRect(x: 0, y: bounds.height - 35, width: bounds.width, height: 35)
+        
+        //Добавляем градиентный слой
+        cellImage.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = CGRect(x: 0, y: cellImage.bounds.height - 35, width: cellImage.bounds.width, height: 35)
+    }
+    
 }
