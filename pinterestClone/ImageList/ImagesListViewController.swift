7aafd33c2fb4e2ea2c7dd2adb3c1becbfe2a6a56
Denis Chakyr <ChakyrIT@gmail.com>
2023-03-31 18:01:10 +0300

    Sprint_09 -> Commit 1
    1) Save for Present View Controller

--- pinterestClone/ImageList/ImagesListViewController.swift
+++ pinterestClone/ImageList/ImagesListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ImageListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
 
     //MARK: - Types
 
@@ -19,6 +19,7 @@ class ImageListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     //MARK: Private Properties
+    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
     private let photosName: [String] = Array(0..<20).map{"\($0)"}
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -40,7 +41,16 @@ class ImageListViewController: UIViewController {
     }
     
     //MARK: - Public methods
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowSingleImageSegueIdentifier {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
     //MARK: - IBAction
     
     //MARK: - Private Methods
@@ -50,12 +60,13 @@ class ImageListViewController: UIViewController {
 
 //MARK: - Extensions
 //MARK: - UITableViewDelegate
-extension ImageListViewController: UITableViewDelegate {
+extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //этот метод ответчает за действия, которые будут выполнены при тапе по ячейке (адрес ячейки содержиться в indexPath и передается в качетсве аргумента)
         if let cell = tableView.cellForRow(at: indexPath) as? ImagesListCell {
             cell.isSelected = false
         }
+        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -73,7 +84,7 @@ extension ImageListViewController: UITableViewDelegate {
 }
 
 //MARK: - UITableViewDataSource
-extension ImageListViewController: UITableViewDataSource {
+extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //данный метод опред. кол. ячеек в секции таблицы
         //Так как секция у нас одна - проигнорируем значение параметра section
@@ -95,7 +106,7 @@ extension ImageListViewController: UITableViewDataSource {
     }
 }
 
-extension ImageListViewController {
+extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         let imageName = "\(indexPath.row)"
         
---
+++ pinterestClone/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+}
