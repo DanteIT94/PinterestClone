f84056ba4d9ef1e15eaa7e6e4cf73bd35fbe3fa9
Denis Chakyr <ChakyrIT@gmail.com>
2023-04-01 11:18:28 +0300

    Sprint 09 - Commit # 2
    Закончили с темой "Навигация"

--- pinterestClone/SingleImage/SingleImageViewController.swift
+++ pinterestClone/SingleImage/SingleImageViewController.swift
@@ -9,9 +9,15 @@ final class SingleImageViewController: UIViewController {
     }
 
     @IBOutlet private var imageView: UIImageView!
-
+    @IBOutlet weak var backButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        backButton.setTitle("", for: .normal)
     }
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
